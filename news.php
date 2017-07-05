25eca7b7c515fcee71a1bf8878f72b92b1efa260
Creemer <3888406@gmail.com>
2017-07-05 18:36:08 +0300

    some refactoring

--- news.php
+++ news.php
@@ -5,11 +5,11 @@ $news = new NewsDB();
 $errMsg = "";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  require "save_news.inc.php";
+    require "save_news.inc.php";
 }
 
 if(isset($_GET['id'])){
-  require "delete_news.inc.php";
+    require "delete_news.inc.php";
 }
 
 ?>
@@ -28,19 +28,23 @@ if(isset($_GET['id'])){
         echo "<h2>ERROR : $errMsg</h2>";
     ?>
     <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
-      <span class="label">Заголовок новости:</span><br />
-      <input type="text" name="title" class="text"/><br />
-      <span class="label">Выберите категорию:</span><br />
-      <select class="label" name="category">
-        <option value="1">Политика</option>
-        <option value="2">Культура</option>
-        <option value="3">Спорт</option>
-      </select>
+        <label class="label">Заголовок новости:
+            <input type="text" name="title" class="text"/><br />
+        </label><br />
+        <label class="label">Выберите категорию:
+            <select class="label" name="category">
+              <option value="1">Политика</option>
+              <option value="2">Культура</option>
+              <option value="3">Спорт</option>
+            </select>
+        </label><br />
       <br />
-      <span class="label">Текст новости:</span><br />
-      <textarea name="description" cols="50" rows="5" class="textarea text"></textarea><br />
-      <span class="label">Источник:</span><br />
-      <input type="text" name="source" class="text"/><br />
+      <label class="label">Текст новости:
+        <textarea name="description" cols="50" rows="5" class="textarea text"></textarea><br />
+      </label><br />
+      <label class="label">Источник:
+          <input type="text" name="source" class="text"/><br />
+      </label><br />
       <br />
       <input type="submit" value="Добавить!" class="sbmButton"/>
     </form>
